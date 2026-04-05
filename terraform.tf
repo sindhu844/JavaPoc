881ffe45c7ff3e51e4914e0d06926ee87d38aaed
sindhu844 <sindhu84401@gmail.com>
2026-04-05 16:57:19 +0530

    deleted awsssh from terraform

--- terraform.tf
+++ terraform.tf
@@ -9,11 +9,6 @@ provider "aws"{
 region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "github-key"
-  public_key = file("id_rsa.pub")
-}
-
 resource "aws_instance" "server_2"{
 ami = "ami-0ec10929233384c7f"
 instance_type = "t2.micro"
